4f509d9bb47275cca7ec5b130c53eb62934cb627
Mark <wei.xiaoma@gmail.com>
2014-04-05 20:30:44 -0700

    adds enumeration for suits

--- .worksheet/src/blackjack.playground.scala
+++ .worksheet/src/blackjack.playground.scala
@@ -1,6 +1,8 @@
 package blackjack
+import Suit._
 
-object playground {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(82); 
-  println("Welcome to the Scala worksheet");$skip(36); 
-  val myCard = new Card(4, "clubs");System.out.println("""myCard  : blackjack.Card = """ + $show(myCard ))}
+object playground {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(96); 
+  println("Welcome to the Scala worksheet");$skip(29); 
+	Suit.values foreach println;$skip(34); 
+  val myCard = new Card(4, Clubs);System.out.println("""myCard  : blackjack.Card = """ + $show(myCard ))}
 }
--- src/blackjack/card.scala
+++ src/blackjack/card.scala
@@ -1,7 +1,14 @@
 package blackjack
 
-class Card(val rank: Int, val suit: String) {
+object Suit extends Enumeration {
+  type Suit = Value
+  val Hearts, Spades, Diamonds, Clubs = Value
+}
+import Suit._
+
+class Card(val rank: Int, val suit: Suit.Value) {
   override def toString: String = {
     rank.toString + " of " + suit.toString
   }
 }
+
