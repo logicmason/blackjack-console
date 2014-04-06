1149ed3ef03c75e1619fdce075322bf8346dfd7d
Mark <wei.xiaoma@gmail.com>
2014-04-05 21:32:42 -0700

    deleted cache so .gitignore works

--- .worksheet/src/playground.scala
+++
@@ -1,4 +0,0 @@
-object playground {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(63); 
-  println("Welcome to the Scala worksheet")}
-  
-}
