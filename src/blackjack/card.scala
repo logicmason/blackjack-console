e8e17101ece016c3283a541810b7891af686326a
Mark <wei.xiaoma@gmail.com>
2014-04-05 21:26:16 -0700

    adds Rank class

--- src/blackjack/card.scala
+++ src/blackjack/card.scala
@@ -1,14 +1,19 @@
 package blackjack
 
+class Card(val rank: Rank, val suit: Suit.Value) {
+  override def toString: String = {
+    rank.toString + " of " + suit.toString
+  }
+}
+
 object Suit extends Enumeration {
   type Suit = Value
   val Hearts, Spades, Diamonds, Clubs = Value
 }
 import Suit._
 
-class Card(val rank: Int, val suit: Suit.Value) {
-  override def toString: String = {
-    rank.toString + " of " + suit.toString
-  }
+class Rank(cardName: String, cardPoints: Int) {
+  val name: String = cardName
+  val points: Int = cardPoints
+  override def toString: String = name
 }
-
