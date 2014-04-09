2338a1edefa8c60b468ee313338e6b416f5c50f9
Mark <wei.xiaoma@gmail.com>
2014-04-08 22:33:50 -0700

    adds deck tests

--- src/blackjack/test/DeckSuite.scala
+++ src/blackjack/test/DeckSuite.scala
@@ -10,13 +10,29 @@ class DeckSuite extends FunSuite {
 
   test("size of new deck") {
     new TestSets {
-      assert(deck.cards.size === 0)
+      assert(deck.cards.size === 52)
     }
   }
 
   test("first card is an ace") {
     new TestSets {
       assert(deck.cards.head.rank.name === "Ace")
+    }
+  }
+
+  test("size of draw pile after drawing a card") {
+    new TestSets {
+      deck.draw
+      assert(deck.pile.size == 51)
+    }
+  }
+
+  test("cards in pile plus those drawn are the same set as the original deck") {
+    new TestSets {
+    	val card1 = deck.draw
+    	val card2 = deck.draw
+    	val pile = deck.pile.toList
+    	assert( (card1 :: card2 :: pile).toSet == deck.cards.toSet )
     }
   }
 }
