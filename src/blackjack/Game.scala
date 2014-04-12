3e5f3ab79dc46159fb67f51212abc17a4ddc8907
Mark <wei.xiaoma@gmail.com>
2014-04-11 23:15:25 -0700

    fixes isBust to use real calculations

--- src/blackjack/Game.scala
+++ src/blackjack/Game.scala
@@ -3,6 +3,7 @@ package blackjack
 class Game(val player: Player) {
 	val deck = new Deck
 	var dealerHand: List[Card] = null
+	val limit = 21
 
 	def gameOver() = {
 	  isBust(dealerHand) || isBust(player.hand)
@@ -18,7 +19,7 @@ class Game(val player: Player) {
 
 	def isBust(hand: List[Card]):Boolean = {
 	  if (hand == null) false
-	  else hand.length > 3  //TODO real check
+	  else tallyLow(hand) > limit
 	}
 }
 
