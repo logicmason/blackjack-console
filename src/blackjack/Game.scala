fbb24ae165098f08f694cf8a149f344ae79b320e
Mark <wei.xiaoma@gmail.com>
2014-04-11 23:14:25 -0700

    adds functions for score calculation

--- src/blackjack/Game.scala
+++ src/blackjack/Game.scala
@@ -8,6 +8,14 @@ class Game(val player: Player) {
 	  isBust(dealerHand) || isBust(player.hand)
 	}
 
+	def tallyLow(hand: List[Card]): Int = {
+	  hand.foldLeft(0)((a,b) => a + (b.rank.minPoints) )
+	}
+
+	def tallyHigh(hand: List[Card]): Int = {
+	  hand.foldLeft(0)((a,b) => a + (b.rank.points))
+	}
+
 	def isBust(hand: List[Card]):Boolean = {
 	  if (hand == null) false
 	  else hand.length > 3  //TODO real check
