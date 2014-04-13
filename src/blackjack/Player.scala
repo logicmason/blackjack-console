7184872a547d8819c36291880c677532f7dbcc61
Mark <wei.xiaoma@gmail.com>
2014-04-12 23:34:20 -0700

    adds bet, wins and loses methods

--- src/blackjack/Player.scala
+++ src/blackjack/Player.scala
@@ -2,6 +2,26 @@ package blackjack
 
 class Player(name: String, var chips: Int) {
 	var hand: List[Card] = null
+	var wager = 0
+
+	def bet(amount:Int): Boolean = {
+	  if (chips < 0) return false
+	  else {
+	    chips -= amount
+	    wager += amount
+	    return true
+	  }
+	}
+
+	def wins {
+	  chips += wager * 2
+	  wager = 0
+	}
+
+	def loses {
+	  wager = 0
+	}
+
 	override def toString: String = s"""
     Name: $name
     Chips: $chips
